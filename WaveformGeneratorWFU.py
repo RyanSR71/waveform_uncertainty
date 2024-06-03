803305aaaa450ff511f06754e73e2dd06f5eb305
RyanSR71 <137436926+RyanSR71@users.noreply.github.com>
2024-06-03 13:37:02 -0700

    Add files via upload

---
+++ WaveformGeneratorWFU.py
@@ -0,0 +1,146 @@
+class WaveformGeneratorWFU(object):
+    duration = PropertyAccessor('_times_and_frequencies', 'duration')
+    sampling_frequency = PropertyAccessor('_times_and_frequencies', 'sampling_frequency')
+    start_time = PropertyAccessor('_times_and_frequencies', 'start_time')
+    frequency_array = PropertyAccessor('_times_and_frequencies', 'frequency_array')
+    time_array = PropertyAccessor('_times_and_frequencies', 'time_array')
+    def __init__(self, duration=None, sampling_frequency=None, start_time=0, frequency_domain_source_model=None,
+                 time_domain_source_model=None, parameters=None,
+                 waveform_uncertainty_nodes=None,dA_sampling=False,dphi_sampling=False,
+                 parameter_conversion=None,
+                 waveform_arguments=None):
+        self._times_and_frequencies = CoupledTimeAndFrequencySeries(duration=duration,
+                                                                    sampling_frequency=sampling_frequency,
+                                                                    start_time=start_time)
+        self.frequency_domain_source_model = frequency_domain_source_model
+        self.time_domain_source_model = time_domain_source_model
+        self.source_parameter_keys = self.__parameters_from_source_model()
+        if parameter_conversion is None:
+            self.parameter_conversion = convert_to_lal_binary_black_hole_parameters
+        else:
+            self.parameter_conversion = parameter_conversion
+        if waveform_arguments is not None:
+            self.waveform_arguments = waveform_arguments
+        else:
+            self.waveform_arguments = dict()
+        if waveform_uncertainty_nodes is not None:
+            self.waveform_uncertainty_nodes = waveform_uncertainty_nodes
+        else:
+            self.waveform_uncertainty_nodes = None
+        if isinstance(parameters, dict):
+            self.parameters = parameters
+        self._cache = dict(parameters=None, waveform=None, model=None)
+        utils.logger.info(
+            "Waveform generator initiated with\n"
+            "  frequency_domain_source_model: {}\n"
+            "  time_domain_source_model: {}\n"
+            "  parameter_conversion: {}"
+            .format(utils.get_function_path(self.frequency_domain_source_model),
+                    utils.get_function_path(self.time_domain_source_model),
+                    utils.get_function_path(self.parameter_conversion))
+        )
+    def __repr__(self):
+        if self.frequency_domain_source_model is not None:
+            fdsm_name = self.frequency_domain_source_model.__name__
+        else:
+            fdsm_name = None
+        if self.time_domain_source_model is not None:
+            tdsm_name = self.time_domain_source_model.__name__
+        else:
+            tdsm_name = None
+        if self.parameter_conversion is None:
+            param_conv_name = None
+        else:
+            param_conv_name = self.parameter_conversion.__name__
+        return self.__class__.__name__ + '(duration={}, sampling_frequency={}, start_time={}, ' \
+                                         'frequency_domain_source_model={}, time_domain_source_model={}, ' \
+                                         'parameter_conversion={}, ' \
+                                         'waveform_uncertainty_nodes={}, ' \
+                                         'waveform_arguments={})'\
+            .format(self.duration, self.sampling_frequency, self.start_time, fdsm_name, tdsm_name,
+                    param_conv_name,self.waveform_uncertainty_nodes, self.waveform_arguments)
+    def frequency_domain_strain(self, parameters=None):
+        return self._calculate_strain(model=self.frequency_domain_source_model,
+                                      model_data_points=self.frequency_array,
+                                      parameters=parameters,
+                                      transformation_function=utils.nfft,
+                                      transformed_model=self.time_domain_source_model,
+                                      transformed_model_data_points=self.time_array,
+                                      waveform_uncertainty_nodes=self.waveform_uncertainty_nodes)
+    def time_domain_strain(self, parameters=None):
+        return self._calculate_strain(model=self.time_domain_source_model,
+                                      model_data_points=self.time_array,
+                                      parameters=parameters,
+                                      transformation_function=utils.infft,
+                                      transformed_model=self.frequency_domain_source_model,
+                                      transformed_model_data_points=self.frequency_array)
+
+    def _calculate_strain(self, model, model_data_points, transformation_function, transformed_model,
+                          transformed_model_data_points, parameters, waveform_uncertainty_nodes):
+        if parameters is not None:
+            self.parameters = parameters
+        if self.parameters == self._cache['parameters'] and self._cache['model'] == model and \
+                self._cache['transformed_model'] == transformed_model:
+            return self._cache['waveform']
+        if model is not None:
+            model_strain = self._strain_from_model(model_data_points, model)
+        elif transformed_model is not None:
+            model_strain = self._strain_from_transformed_model(transformed_model_data_points, transformed_model,
+                                                               transformation_function)
+        else:
+            raise RuntimeError("No source model given")
+        self._cache['waveform'] = model_strain
+        self._cache['parameters'] = self.parameters.copy()
+        self._cache['model'] = model
+        self._cache['transformed_model'] = transformed_model
+        if self.waveform_uncertainty_nodes is not None:
+            alphas = np.array()
+            betas = np.array()
+            for i in range(len(self.waveform_uncertainty_nodes)):
+                if dA_sampling == True:
+                    alphas[i]=parameters['alpha_' + f'{i+1}']
+                else:
+                    alphas[i]=0
+                if dphi_sampling == True:
+                    betas[i]=parameters['beta_' + f'{i+1}']
+                else:
+                    betas[i]=0
+            dA = scipy.interpolate.CubicSpline(self.waveform_uncertainty_nodes,alphas)(self.frequency_array)
+            dphi = scipy.interpolate.CubicSpline(self.waveform_uncertainty_nodes,betas)(self.frequency_array)
+            model_strain['plus'] *= (1+dA)*(2+dphi*1j)/(2-dphi*1j)
+            model_strain['cross'] *= (1+dA)*(2+dphi*1j)/(2-dphi*1j)
+        return model_strain
+    def _strain_from_model(self, model_data_points, model):
+        return model(model_data_points, **self.parameters)
+    def _strain_from_transformed_model(self, transformed_model_data_points, transformed_model, transformation_function):
+        transformed_model_strain = self._strain_from_model(transformed_model_data_points, transformed_model)
+        if isinstance(transformed_model_strain, np.ndarray):
+            return transformation_function(transformed_model_strain, self.sampling_frequency)
+        model_strain = dict()
+        for key in transformed_model_strain:
+            if transformation_function == utils.nfft:
+                model_strain[key], _ = \
+                    transformation_function(transformed_model_strain[key], self.sampling_frequency)
+            else:
+                model_strain[key] = transformation_function(transformed_model_strain[key], self.sampling_frequency)
+        return model_strain
+    @property
+    def parameters(self):
+        return self.__parameters
+    @parameters.setter 
+    def parameters(self, parameters):
+        if not isinstance(parameters, dict):
+            raise TypeError('"parameters" must be a dictionary.')
+        new_parameters = parameters.copy()
+        new_parameters, _ = self.parameter_conversion(new_parameters)
+        self.__parameters = new_parameters
+        self.__parameters.update(self.waveform_arguments)
+    def __parameters_from_source_model(self):
+        if self.frequency_domain_source_model is not None:
+            model = self.frequency_domain_source_model
+        elif self.time_domain_source_model is not None:
+            model = self.time_domain_source_model
+        else:
+            raise AttributeError('Either time or frequency domain source '
+                                 'model must be provided.')
+        return set(utils.infer_parameters_from_function(model))
