8b68265038ebeeeffd619574d2034ef0d34414bd
Ryan Johnson <137436926+RyanSR71@users.noreply.github.com>
2024-07-11 18:34:29 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,11 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+  ...
+  "footer_items": ["sphinx-version"],
+  ...
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
