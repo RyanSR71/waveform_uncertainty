a2af3299f6e0fe99d4d9c7d4fcdf1f76291220f3
Ryan Johnson <137436926+RyanSR71@users.noreply.github.com>
2024-07-15 13:28:14 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ project = 'WaveformUncertainty'
 copyright = '2024, Ryan Johnson, No Rights Reserved'
 author = 'Ryan Johnson'
 
-release = ''
+release = 'beta'
 version = '0.1.2'
 
 # -- General configuration
