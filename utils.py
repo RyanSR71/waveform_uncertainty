07259fa77425ac3094e6f57eb5c169a19f9a5487
Ryan Johnson <137436926+RyanSR71@users.noreply.github.com>
2024-10-28 14:42:30 -0700

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,48 @@
+import numpy as np
+import bilby
+import random
+import time as tm
+import sys
+import scipy
+import lal
+from bilby.core import utils
+from bilby.core.series import CoupledTimeAndFrequencySeries
+from bilby.core.utils import PropertyAccessor
+from bilby.gw.conversion import convert_to_lal_binary_neutron_star_parameters
+
+
+
+def injection(data,**kwargs):
+    '''
+    Pulls a sample out of a parameter dictionary
+    
+    Parameters
+    ==================
+    data: dictionary
+        dictionary of parameter samples
+    index: int, optional
+        position within the dict to pull the sample
+        default: random integer between zero and the length of the data
+        
+    Returns
+    ==================
+    injection: dictionary
+        dictionary of injection parameters
+    ''' 
+    index = kwargs.get('index',random.randint(0,len(data)))
+    
+    injection = dict()
+    for key in data.keys():
+        injection[f'{key}']=data[f'{key}'][index]
+        
+    return injection
+
+
+
+def progressBar(count_value, total, suffix=''): #To Do
+        bar_length = 100
+        filled_up_Length = int(round(bar_length* count_value / float(total)))
+        percentage = round(100.00 * count_value/float(total),1)
+        bar = '=' * filled_up_Length + '-' * (bar_length - filled_up_Length)
+        sys.stdout.write('[%s] %s%s %s\r' %(bar, percentage, '%', suffix))
+        sys.stdout.flush()
