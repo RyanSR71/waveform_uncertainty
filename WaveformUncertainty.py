a87599b9aac91788417610def4b966140eb5d9fb
Ryan Johnson <137436926+RyanSR71@users.noreply.github.com>
2025-01-14 01:00:53 -0800

    Update WaveformUncertainty.py

--- WaveformUncertainty.py
+++ WaveformUncertainty.py
@@ -1,5 +1,5 @@
 "WaveformUncertainty package"
-__version__ = "0.8.4.0"
+__version__ = "0.9.0.0"
 
 import numpy as np
 import bilby
@@ -626,6 +626,257 @@ def Q_factor(WFU_result,NHP_result,injection):
 
 
 class WaveformGeneratorWFU(object):
+    '''
+    Modified WaveformGenerator object from bilby.gw to include waveform uncertainty corrections in the strain calculation
+    To sample waveform uncertainty, include all relevant "alpha" and "phi" parameters in the prior.
+    Note: make sure the number of alphas, phis, and waveform_uncertainty_nodes are the same
+    
+    New Parameters
+    ==================
+    waveform_uncertainty_nodes: numpy.ndarray, optional
+        array of frequency nodes to be used in generating the dA and dphi splines
+        default: None
+    dA_sampling: bool, optional
+        if True, the waveform generator will attempt to pull alpha parameters from the parameter dictionary (either an injection or the prior)
+        default: None
+    dphi_sampling: bool, optional
+        if True, the waveform generator will attempt to pull phi parameters from the parameter dictionary (either an injection or the prior)
+        default: None
+    indexes: numpy.array, optional
+        the numbers of waveform correction parameters; e.g. phi_1 = 1, alpha_-8 = -8, etc.
+        default: None
+    '''
+    duration = PropertyAccessor('_times_and_frequencies', 'duration')
+    sampling_frequency = PropertyAccessor('_times_and_frequencies', 'sampling_frequency')
+    start_time = PropertyAccessor('_times_and_frequencies', 'start_time')
+    frequency_array = PropertyAccessor('_times_and_frequencies', 'frequency_array')
+    time_array = PropertyAccessor('_times_and_frequencies', 'time_array')
+    def __init__(self, duration=None, sampling_frequency=None, start_time=0, frequency_domain_source_model=None,
+                 time_domain_source_model=None, parameters=None,
+                 frequency_nodes=None,indexes=None,correct_amplitude=False,correct_phase=True,
+                 geometrized=True,parameter_conversion=None,
+                 waveform_arguments=None):
+        self._times_and_frequencies = CoupledTimeAndFrequencySeries(duration=duration,
+                                                                    sampling_frequency=sampling_frequency,
+                                                                    start_time=start_time)
+        self.frequency_domain_source_model = frequency_domain_source_model
+        self.time_domain_source_model = time_domain_source_model
+        self.source_parameter_keys = self.__parameters_from_source_model()
+        self.indexes=indexes
+        
+        if parameter_conversion is None:
+            self.parameter_conversion = convert_to_lal_binary_black_hole_parameters
+        else:
+            self.parameter_conversion = parameter_conversion
+        if waveform_arguments is not None:
+            self.waveform_arguments = waveform_arguments
+        else:
+            self.waveform_arguments = dict()
+        if frequency_nodes is not None:
+            self.frequency_nodes = frequency_nodes
+        else:
+            self.frequency_nodes = None
+        if isinstance(parameters, dict):
+            self.parameters = parameters
+        self._cache = dict(parameters=None, waveform=None, model=None)
+        
+        utils.logger.info(
+            "Waveform generator initiated with\n"
+            "  frequency_domain_source_model: {}\n"
+            "  time_domain_source_model: {}\n"
+            "  parameter_conversion: {}"
+            .format(utils.get_function_path(self.frequency_domain_source_model),
+                    utils.get_function_path(self.time_domain_source_model),
+                    utils.get_function_path(self.parameter_conversion))
+        )
+        
+    def __repr__(self):
+        if self.frequency_domain_source_model is not None:
+            fdsm_name = self.frequency_domain_source_model.__name__
+        else:
+            fdsm_name = None
+        if self.time_domain_source_model is not None:
+            tdsm_name = self.time_domain_source_model.__name__
+        else:
+            tdsm_name = None
+        if self.parameter_conversion is None:
+            param_conv_name = None
+        else:
+            param_conv_name = self.parameter_conversion.__name__
+        return self.__class__.__name__ + '(duration={}, sampling_frequency={}, start_time={}, ' \
+                                         'frequency_domain_source_model={}, time_domain_source_model={}, ' \
+                                         'parameter_conversion={}, ' \
+                                         'frequency_nodes={}, ' \
+                                         'indexes={}, ' \
+                                         'waveform_arguments={})'\
+            .format(self.duration, self.sampling_frequency, self.start_time, fdsm_name, tdsm_name,
+                    param_conv_name, self.frequency_nodes, self.indexes, self.waveform_arguments)
+    
+    def frequency_domain_strain(self, parameters=None):
+        return self._calculate_strain(model=self.frequency_domain_source_model,
+                                      model_data_points=self.frequency_array,
+                                      parameters=parameters,
+                                      transformation_function=utils.nfft,
+                                      transformed_model=self.time_domain_source_model,
+                                      transformed_model_data_points=self.time_array,
+                                      frequency_nodes=self.frequency_nodes,
+                                      indexes=self.indexes
+                                      )
+
+    def time_domain_strain(self,parameters=None):
+        fd_model_strain = self._calculate_strain(model=self.frequency_domain_source_model,
+                                      model_data_points=self.frequency_array,
+                                      parameters=parameters,
+                                      transformation_function=utils.nfft,
+                                      transformed_model=self.time_domain_source_model,
+                                      transformed_model_data_points=self.time_array,
+                                      frequency_nodes=self.frequency_nodes,
+                                      indexes=self.indexes
+                                      )
+        
+        plus_td_waveform = self.sampling_frequency*np.fft.ifft(fd_model_strain['plus'])
+        plus_td_model_strain = np.interp(self.time_array,np.linspace(self.time_array[0],self.time_array[-1],len(plus_td_waveform)),plus_td_waveform)
+
+        cross_td_waveform = self.sampling_frequency*np.fft.ifft(fd_model_strain['cross'])
+        cross_td_model_strain = np.interp(self.time_array,np.linspace(self.time_array[0],self.time_array[-1],len(cross_td_waveform)),cross_td_waveform)
+
+        model_strain = dict()
+        model_strain['plus'] = plus_td_model_strain
+        model_strain['cross'] = cross_td_model_strain
+
+        return model_strain
+    
+    def _calculate_strain(self, model, model_data_points, transformation_function, transformed_model,
+                          transformed_model_data_points, parameters, frequency_nodes,indexes):
+        if parameters is not None:
+            self.parameters = parameters
+        if self.parameters == self._cache['parameters'] and self._cache['model'] == model and \
+                self._cache['transformed_model'] == transformed_model:
+            return self._cache['waveform']
+        if model is not None:
+            model_strain = self._strain_from_model(model_data_points, model)
+        elif transformed_model is not None:
+            model_strain = self._strain_from_transformed_model(transformed_model_data_points, transformed_model,
+                                                               transformation_function)
+        else:
+            raise RuntimeError("No source model given")
+        self._cache['waveform'] = model_strain
+        self._cache['parameters'] = self.parameters.copy()
+        self._cache['model'] = model
+        self._cache['transformed_model'] = transformed_model
+        
+        '''
+        The following block performs the waveform uncertainty correction:
+        '''
+        
+        if self.frequency_nodes is not None:
+            
+            if self.indexes is not None and len(self.frequency_nodes) != len(self.indexes):
+                raise Exception("Frequency Nodes Do Not Match Provided Indexes")
+            
+            if self.geometrized is True:
+                
+                c = 299792458
+                M = bilby.gw.conversion.generate_mass_parameters(parameters)['total_mass']*lal.MSUN_SI
+                
+                temp_Mf_grid = np.linspace(self.frequency_nodes[0],self.frequency_nodes[-1],1000)
+                temp_frequency_grid = (temp_Mf_grid*c**3)/(lal.G_SI*M)
+                
+                try:
+                    alphas = [parameters[f'dA_{i}'] for i in self.indexes]
+                    temp_dA = scipy.interpolate.CubicSpline(self.frequency_nodes,alphas)(temp_Mf_grid)
+                    dA = np.interp(self.frequency_array,temp_frequency_grid,temp_dA)
+                except:
+                    dA = 0
+                    if self.correct_amplitude is True:
+                        raise Exception('Amplitude Correction Failed!')
+               
+                try:
+                    phis = [parameters[f'dphi_{i}'] for i in self.indexes]
+                    temp_dphi = scipy.interpolate.CubicSpline(self.frequency_nodes,phis)(temp_Mf_grid)
+                    dphi = np.interp(self.frequency_array,temp_frequency_grid,temp_dphi)
+                except:
+                    dphi = 0
+                    if self.correct_phase is True:
+                        raise Exception('Phase Correction Failed!')
+
+            else:
+                
+                try:
+                    alphas = [parameters[f'dA_{i}'] for i in indexes]
+                    dA = scipy.interpolate.CubicSpline(self.frequency_nodes,alphas)(self.frequency_array)
+                except:
+                    dA = 0
+                    if self.correct_amplitude is True:
+                        raise Exception('Amplitude Correction Failed!')
+               
+                try:
+                    phis = [parameters[f'dphi_{i}'] for i in indexes]
+                    dphi = scipy.interpolate.CubicSpline(self.frequency_nodes,phis)(self.frequency_array)
+                except:
+                    dphi = 0
+                    if self.correct_phase is True:
+                        raise Exception('Phase Correction Failed!')
+                
+                
+            model_strain['plus'] = model_strain['plus']*(1+dA)*np.exp(dphi*1j)
+            model_strain['cross'] = model_strain['cross']*(1+dA)*np.exp(dphi*1j)
+            
+        return model_strain
+    
+    def _strain_from_model(self, model_data_points, model):
+        return model(model_data_points, **self.parameters)
+    
+    def _strain_from_transformed_model(self, transformed_model_data_points, transformed_model, transformation_function):
+        transformed_model_strain = self._strain_from_model(transformed_model_data_points, transformed_model)
+        if isinstance(transformed_model_strain, np.ndarray):
+            return transformation_function(transformed_model_strain, self.sampling_frequency)
+        model_strain = dict()
+        for key in transformed_model_strain:
+            if transformation_function == utils.nfft:
+                model_strain[key], _ = \
+                    transformation_function(transformed_model_strain[key], self.sampling_frequency)
+            else:
+                model_strain[key] = transformation_function(transformed_model_strain[key], self.sampling_frequency)
+        return model_strain
+    
+    @property
+    def parameters(self):
+        return self.__parameters
+    
+    @parameters.setter 
+    def parameters(self, parameters):
+        if not isinstance(parameters, dict):
+            raise TypeError('"parameters" must be a dictionary.')
+        new_parameters = parameters.copy()
+        new_parameters, _ = self.parameter_conversion(new_parameters)
+        for key in self.source_parameter_keys.symmetric_difference(new_parameters):
+            # preventing waveform uncertainty parameters from being removed
+            if self.frequency_nodes is not None:
+                if self.indexes is not None:
+                    indexes = self.indexes
+                else:
+                    indexes = list(range(1,len(self.frequency_nodes)+1))
+                if key not in [f'dA_{i}' for i in indexes]+[f'dphi_{i}' for i in indexes]:  
+                    new_parameters.pop(key)
+            else:
+                new_parameters.pop(key)
+        self.__parameters = new_parameters
+        self.__parameters.update(self.waveform_arguments)
+        
+    def __parameters_from_source_model(self):
+        if self.frequency_domain_source_model is not None:
+            model = self.frequency_domain_source_model
+        elif self.time_domain_source_model is not None:
+            model = self.time_domain_source_model
+        else:
+            raise AttributeError('Either time or frequency domain source '
+                                 'model must be provided.')
+        return set(utils.infer_parameters_from_function(model))
+
+
+
+class WaveformGeneratorWFU_old(object):
     '''
     Modified WaveformGenerator object from bilby.gw to include waveform uncertainty corrections in the strain calculation
     To sample waveform uncertainty, include all relevant "alpha" and "phi" parameters in the prior.
