bf5003041726c3bd64d5f696b145e248e947fd0c
Ryan Johnson <137436926+RyanSR71@users.noreply.github.com>
2025-06-11 16:30:38 -0700

    Update __init__.py

--- WaveformUncertainty/__init__.py
+++ WaveformUncertainty/__init__.py
@@ -1,4 +1,4 @@
 "WaveformUncertainty package"
-__version__ = "0.12.5"
+__version__ = "0.13.0"
 
-from . import parameterization, prior, waveform_generator, utils
+from . import parameterization, prior, sampler, waveform_generator, utils
